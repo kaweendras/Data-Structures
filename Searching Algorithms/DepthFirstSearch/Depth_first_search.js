8be49f8f4f1ba6b5e9c194e40e7539ca8ad3ebcf
Luiz Carneiro <luuizpaulo@gmail.com>
2020-10-25 17:46:31 +0100

    Added tests for using the tree and its methods

--- Searching Algorithms/DepthFirstSearch/Depth_first_search.js
+++ Searching Algorithms/DepthFirstSearch/Depth_first_search.js
@@ -1,22 +1,110 @@
 class Node {
-  constructor(name) {
-    this.name = name;
-    this.children = [];
+  constructor(data) {
+    this.data = data;
+    this.children = []
   }
 
-  addChild(name) {
-    this.children.push(new Node(name));
-    return this;
+  add(data) {
+    this.children.push(new Node(data))
   }
 
-  depthFirstSearch(array = []) {
-    // Speed - O(n) and Space - O(n)
-    array.push(this.name)
-    for (let node of this.children) {
-      node.depthFirstSearch(array)
-    }
-    return array
+  remove(data) {
+    this.children = this.children.filter(node => node.data !== data)
   }
 }
 
-exports.Node = Node;
+class Tree {
+  constructor() {
+    this.root = null
+  }
+
+  traverseBF(fn) {
+    const arr = [this.root]
+    while (arr.length) {
+      const node = arr.shift()
+      arr.push(...node.children)
+      fn(node)
+    }
+  }
+
+  traverseDF(fn) {
+    const arr = [this.root]
+    while (arr.length) {
+      const node = arr.shift()
+      arr.unshift(...node.children)
+      fn(node)
+    }
+  }
+}
+
+module.exports = { Tree, Node };
+
+
+// Tests 
+// const T = require('./index');
+// const Node = T.Node;
+// const Tree = T.Tree;
+
+// describe('Node', () => {
+//   test('Node is a constructor', () => {
+//     expect(typeof Node.prototype.constructor).toEqual('function');
+//   });
+
+//   test('Node has a data and children properties', () => {
+//     const n = new Node('a');
+//     expect(n.data).toEqual('a');
+//     expect(n.children.length).toEqual(0);
+//   });
+
+//   test('Node can add children', () => {
+//     const n = new Node('a');
+//     n.add('b');
+//     expect(n.children.length).toEqual(1);
+//     expect(n.children[0].children).toEqual([]);
+//   });
+
+//   test('Node can remove children', () => {
+//     const n = new Node('a');
+//     n.add('b');
+//     expect(n.children.length).toEqual(1);
+//     n.remove('b');
+//     expect(n.children.length).toEqual(0);
+//   });
+// });
+
+// describe('Tree', () => {
+//   test('starts empty', () => {
+//     const t = new Tree();
+//     expect(t.root).toEqual(null);
+//   });
+
+//   test('Can traverse bf', () => {
+//     const letters = [];
+//     const t = new Tree();
+//     t.root = new Node('a');
+//     t.root.add('b');
+//     t.root.add('c');
+//     t.root.children[0].add('d');
+
+//     t.traverseBF(node => {
+//       letters.push(node.data);
+//     });
+
+//     expect(letters).toEqual(['a', 'b', 'c', 'd']);
+//   });
+
+//   test('Can traverse DF', () => {
+//     const letters = [];
+//     const t = new Tree();
+//     t.root = new Node('a');
+//     t.root.add('b');
+//     t.root.add('d');
+//     t.root.children[0].add('c');
+
+//     t.traverseDF(node => {
+//       letters.push(node.data);
+//     });
+
+//     expect(letters).toEqual(['a', 'b', 'c', 'd']);
+//   });
+// });
